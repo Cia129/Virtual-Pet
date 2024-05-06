637b547411be70a198ab86d543330ea6b663f64d
Riya <rk09771p@pace.edu>
2024-05-06 13:01:42 -0400

    Done

---
+++ VirtualPet.java
@@ -0,0 +1,11 @@
+public class DogNeeds {
+
+    //main needs of our virtual dog (what we need to do to fulfill them alive!)
+    private String dogName;
+    private int hunger;
+    private int thirst;
+    private int attention;
+    private int bladder;
+    private int energy;
+    private int hygiene;
+}
